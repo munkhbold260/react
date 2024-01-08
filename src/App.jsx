46556bd67804dbf35c5bbe4ae5907dc57bebcdc7
bold_max <munkhbold260@gmail.com>
2024-01-08 22:40:27 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Card from "./components/Card";
+
+export default function App() {
+  return (
+    <div>
+      <p>Hello World</p>
+      <Card />
+    </div>
+  );
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+const Card = () => {
+  return (
+    <div>
+      <p>Hello Card</p>
+    </div>
+  );
+};
+
+export default Card;
